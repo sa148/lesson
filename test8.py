70182188e334f5732c044636b610d1fa6e0855c8
sa148 <s.kurono@outlook.jp>
2023-01-08 03:19:46 +0900

    add包含条件式

--- test8.py
+++ test8.py
@@ -2,3 +2,8 @@ a = 10
 b = 20
 result = a == b
 print(result)
+
+c = 15
+d = 15
+result = c == d
+print(result)
---
+++ test9.py
@@ -0,0 +1,7 @@
+x = 'A'
+result = x in 'Apple'
+print(result)
+
+x = 'B'
+result = x in 'Apple'
+print(result)
