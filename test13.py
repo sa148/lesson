78a1e9ad72d747adf11d36f36091c3eeb8135066
sa148 <s.kurono@outlook.jp>
2023-01-08 18:21:09 +0900

    addイニシャライザに値を設定

--- test13.py
+++ test13.py
@@ -3,5 +3,29 @@ class BodyCondiction:
     def bmi_calc(self):
         print('あいうえお')
 
+# インスタンス代入 #
 bc = BodyCondiction()
 bc.bmi_calc()
+
+# イニシャライザ実行確認 #
+class BodyCondiction:
+    def __init__(self):
+        print('初期化')
+
+    def bmi_calc(self):
+        print('あいうえお')
+
+bc = BodyCondiction()
+
+# イニシャライザ中に値を設定 # 
+class BodyCondiction:
+    def __init__(self, arg_weight, arg_height):
+        self.weight = arg_weight
+        self.height = arg_height
+    
+    def bmi_calc(self):
+        print('あいうえお')
+
+bc = BodyCondiction(55, 150)
+print(bc.weight)
+print(bc.height)
