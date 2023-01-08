0ec294a266f3b4a5f11fa92561d1b10f1625dec7
sa148 <s.kurono@outlook.jp>
2023-01-08 17:27:46 +0900

    add関数基本

---
+++ test11.py
@@ -0,0 +1,7 @@
+age = 30
+result = not (age >= 20)
+print(result)
+
+age = 19
+result = not (age >= 20)
+print(result)
---
+++ test12.py
@@ -0,0 +1,5 @@
+def print_banana():
+    # ここからが関数の処理部分 #
+    print('banana')
+    
+print_banana()
