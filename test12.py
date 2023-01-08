bae2b456ada07cc59df3b766feb1526f9fec9480
sa148 <s.kurono@outlook.jp>
2023-01-08 17:38:14 +0900

    add引数と戻り値

--- test12.py
+++ test12.py
@@ -4,8 +4,18 @@ def print_banana():
 
 print_banana()
 
+# 下が引数使用 #
 def print_text(text):
     # ここからが関数の処理部分 #
     print(text)
 
 print_text('apple')
+
+# 引数と戻り設定 #
+
+def question_text(text):
+    text_q = text + '?'
+    return text_q
+
+result_text = question_text('apple')
+print(result_text)
