9d0c0dabc9cb6a4c725a8125e638e5e5aea4fa9e
sa148 <s.kurono@outlook.jp>
2023-01-09 15:57:00 +0900

    add例外処理２つ

--- test15.py
+++ test15.py
@@ -1,3 +1,19 @@
+# 例外が発生しない処理 #
+x = 3
+y = 3
+
+try:
+    result = x / y
+except ZeroDivisionError as e:
+    print('ゼロで割れない')
+    print(e)
+except NameError as e:
+    print('変数が定義されていません')
+    print(e)
+else:
+  print(result)
+  print('正常に処理されました')
+
 # finallyを追加 #
 x = 2
 y = 0
