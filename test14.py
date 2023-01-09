f2df5082cd28502e695c91fdb60d48077c69966d
sa148 <s.kurono@outlook.jp>
2023-01-09 15:43:30 +0900

    add変数代入

--- test14.py
+++ test14.py
@@ -6,3 +6,14 @@ try:
     result = x / y
 except ZeroDivisionError:
     print('ゼロで割ることはできません')
+
+# 例外オブジェクトを変数に代入 #
+x = 1
+y = 0
+
+try:
+    result = x / y
+except ZeroDivisionError as e:
+    print('ゼロで割ることはできません')
+    print(e)
+
