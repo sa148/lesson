d2a8e7abce7bc2a1f95db724a4677826907ccaad
sa148 <s.kurono@outlook.jp>
2023-01-11 04:54:12 +0900

    addjoinメソッド

--- test24.py
+++ test24.py
@@ -2,3 +2,10 @@
 x_list = ['apple', 'orende', 'banana']
 with open('test3.txt', 'w') as f:
     f.writelines(x_list)
+
+# joinメソッド #
+x_list = ['apple', 'orende', 'banana']
+s = '\n'.join(x_list)
+
+with open('test.txt', 'w') as f:
+    f.write(s)
