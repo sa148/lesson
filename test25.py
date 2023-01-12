133fd5b758037031cfa2e3d37dccdf03d23cff1d
sa148 <s.kurono@outlook.jp>
2023-01-13 03:06:26 +0900

    add差集合

--- test25.py
+++ test25.py
@@ -15,3 +15,13 @@ y_set = set(y_list)
 
 result = x_set & y_set
 print(result)
+
+# 差集合 #
+x_set = {11, 222, 333}
+
+y_list = [11, 222, 444, 555, 11]
+y_set = set(y_list)
+
+result = x_set - y_set
+print(result)
+
