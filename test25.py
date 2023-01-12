aa6b7d31f68856e66b8ec6e812067c77e4168b0f
sa148 <s.kurono@outlook.jp>
2023-01-13 03:07:34 +0900

    add和集合

--- test25.py
+++ test25.py
@@ -25,3 +25,11 @@ y_set = set(y_list)
 result = x_set - y_set
 print(result)
 
+# 和集合 #
+x_set = {11, 222, 333}
+
+y_list = [11, 222, 444, 555, 11]
+y_set = set(y_list)
+
+result = x_set | y_set  
+print(result)
