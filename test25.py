cf1b4483c3405e28fa7a4750354a605c7d5ce964
sa148 <s.kurono@outlook.jp>
2023-01-13 03:05:20 +0900

    add積集合

--- test25.py
+++ test25.py
@@ -6,3 +6,12 @@ y_set = set(y_list)
 
 print(x_set)
 print(y_set)
+
+# 積集合を取得 #
+x_set = {11, 222, 333}
+
+y_list = [11, 222, 444, 555, 11]
+y_set = set(y_list)
+
+result = x_set & y_set
+print(result)
