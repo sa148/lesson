113545ab4b9ada7a04d3464e84b01fb2762c744c
sa148 <s.kurono@outlook.jp>
2023-01-14 04:38:43 +0900

    [add]ta

---
+++ test30.py
@@ -0,0 +1,15 @@
+# 多重継承 #
+class WizardClass:
+    def __init__(self):
+      print('WizardClassのinit')
+
+class SwordFighterClass:
+    def __init__(self):
+      print('SwordFighterClassのinit')
+
+class MagicSwordFighterClass(WizardClass, 
+                            SwordFighterClass):
+
+    pass
+
+msf = MagicSwordFighterClass()
