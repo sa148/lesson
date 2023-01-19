ac164bb8588b770a0cef344084367ca08beeb2df
sa148 <s.kurono@outlook.jp>
2023-01-19 23:04:37 +0900

    add関数の呼び出し側で引数名前を指定して値を指定

--- test33.py
+++ test33.py
@@ -13,3 +13,11 @@ def total_price_1item(unit_price, quantity = 1):
 
 total_price = total_price_1item(120)
 print(total_price)
+
+# 関数の呼び出し側で引数名前を指定して値を指定 #
+def total_price_1item(unit_price, quantity = 1):
+    total_price = unit_price * quantity
+    return total_price
+
+total_price = total_price_1item(quantity=20, unit_price=130)
+print(total_price)
