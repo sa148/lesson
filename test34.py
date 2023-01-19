65207306a7dff3e02b00e1e19541aa3fb3d12547
sa148 <s.kurono@outlook.jp>
2023-01-19 23:20:24 +0900

    addタイプアノテーションとデフォルト値

--- test34.py
+++ test34.py
@@ -13,3 +13,11 @@ def total_price_1item(unit_price: int, quantity: int)-> int:
 
 total_price = total_price_1item(130, '1')
 print(total_price)
+
+# タイプアノテーション+デフォルト値 #
+def total_price_1item(unit_price: int, quantity: int = 1)-> str:
+    total_price = unit_price * quantity
+    return f'{total_price:,}'
+
+total_price = total_price_1item(1300)
+print(total_price)
