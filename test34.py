63f0ee6d71ead8010b961dba3a570d669ba37b97
sa148 <s.kurono@outlook.jp>
2023-01-19 23:15:47 +0900

    add繰り返す

--- test34.py
+++ test34.py
@@ -6,3 +6,10 @@ def total_price_1item(unit_price: int, quantity: int)-> int:
 total_price = total_price_1item(130, 3)
 print(total_price)
 
+# 繰り返す #
+def total_price_1item(unit_price: int, quantity: int)-> int:
+    total_price = unit_price * quantity
+    return total_price
+
+total_price = total_price_1item(130, '1')
+print(total_price)
