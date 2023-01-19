78a73941ba73c4120e58693adc84f16ff4af0205
sa148 <s.kurono@outlook.jp>
2023-01-19 22:37:42 +0900

    add地名

--- test32.py
+++ test32.py
@@ -15,10 +15,3 @@ print(x)
 foods = ['apple', 'banana', 'lemon']
 x = [i for i in foods if 'a' in i]
 print(x)
-
-# 三項演算子 #
-x1 = [i for i in range(6)]
-x2 = ['ぐ' if i % 2 == 0 else i for i in range(6)]
-print(x1)
-print(x2)
-
