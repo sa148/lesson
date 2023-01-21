963561904231bad959938e850ad4f105b68df7c1
sa148 <s.kurono@outlook.jp>
2023-01-21 22:28:02 +0900

    addファイル書き出し

--- test39.py
+++ test39.py
@@ -9,11 +9,25 @@ Logger.serLevel(handler)
 Logger.debug('これはデバッグログです')
 
 # ログフォーマット #
-from Logging.config import getLogger, StreamHandler, DEBUG
+from Logging.config import getLogger, StreamHandler, Fotmatter, DEBUG
 
 formatter = Formatter('[%(levelname)s] %(asctime)s - %(messeage)s (%(filename)s)')
 Logger = getLogger(__name__)
 handler = StreamHandler()
+handler.setLevel(DEBUG)
+handler.setFormtter(formatter)
+Logger.setLevel(DEBUG)
+Logger.serLevel(handler)
+
+Logger.debug('入力値は1000までです')
+Logger.error('ファイルが存在していません')
+
+# ファイル書き出し #
+from Logging.config import getLogger, FireHandler, Formtter, DEBUG
+
+formatter = Formatter('[%(levelname)s] %(asctime)s - %(messeage)s (%(filename)s)')
+Logger = getLogger(__name__)
+handler = StreamHandler('Log.txt')
 handler.setLevel(DEBUG)
 handler.setFormtter(formatter)
 Logger.setLevel(DEBUG)
