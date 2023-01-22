99dbbd1b322b3c578c4dea62ac5e9226d04f1f11
sa148 <s.kurono@outlook.jp>
2023-01-22 22:46:58 +0900

    add日付計算

--- test42.py
+++ test42.py
@@ -2,3 +2,11 @@
 from datetime import datetime
 n = datetime.now()
 print(n)
+
+# 日付の計算 #
+from datetime import datetime, timedelta
+d1 = datetime(2023, 1, 1)
+d2 = datetime(2023, 1, 22)
+result = d1 - d2
+print(result)
+print(result.days)
