55253c4dd7923c6ab91a0e9c9224cafcc50a6baa
sa148 <s.kurono@outlook.jp>
2023-01-22 23:08:14 +0900

    add祝日調べる

--- test43.py
+++ test43.py
@@ -3,3 +3,12 @@ from datetime import datetime, timedelta, timezone
 JST = timezone(timedelta(hours=+9))
 result = datetime(2023, 1, 22, 1, 1, 1, tzinfo=JST)
 print(result)
+# 祝日か調べる #
+import jpholiday 
+from datetime import datetime
+
+d = datetime(2023, 1, 22)
+result = jpholiday.is_holiday(d)
+print(result)
+
+# 祝日全て #
