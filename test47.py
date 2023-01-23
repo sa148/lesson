3dc6b362f6f8916643a0625689e9462aa04a03a6
sa148 <s.kurono@outlook.jp>
2023-01-24 02:59:24 +0900

    addフィールド値の検証true版

--- test47.py
+++ test47.py
@@ -7,3 +7,15 @@ class User:
 user1 = User('kato', 20)
 user2 = User('kato', 20)
 print(user1 == user2)
+
+# ==でフィールド値の検証true版 #
+from dataclasses import dataclass
+
+@dataclass(frozen=True)
+class User:
+    name: str
+    age: int
+
+user1 = User('kato', 20)
+user2 = User('kato', 20)
+print(user1 == user2)
