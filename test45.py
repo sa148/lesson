3cee7fadf25f96d0d3af9b2cb462e23c0fdf373f
sa148 <s.kurono@outlook.jp>
2023-01-23 14:10:34 +0900

    add辞書で受け取る可変長引数

--- test45.py
+++ test45.py
@@ -5,3 +5,10 @@ def func(*args, x):
 
 func('あ', x=1)
 func('あ', 'A', 'a', x=2)
+
+# 辞書で受け取る可変長引数 #
+def func(**kwargs):
+    print(kwargs)
+
+func(name='斎藤', user_id=222)
+func(item='牛乳', item_id=111, price=100)
