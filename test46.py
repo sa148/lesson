a41aeb9b186a230cef2613e65397ab9c4e26a588
sa148 <s.kurono@outlook.jp>
2023-01-24 02:43:06 +0900

    additems

--- test46.py
+++ test46.py
@@ -9,3 +9,15 @@ class User:
 user = User('sato', 20)
 print(user.name)
 print(user.age)
+
+# items追加 #
+from dataclasses import dataclass, field
+
+@dataclass
+class User:
+    name: str
+    age: int
+    items: list[int] = field(default_factory=lambda: ['note', 'pen'])
+
+user = User('sato', 20)
+print(user.items)
