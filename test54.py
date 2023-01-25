1944687a26747aecc95d5ddf2e670a8a78ba21f4
sa148 <s.kurono@outlook.jp>
2023-01-26 03:39:50 +0900

    add罫線

--- test54.py
+++ test54.py
@@ -5,3 +5,9 @@ sheet_name = wb.sheetnames[0]
 ws1 = wb[sheet_name]
 ws1.marge_cells('A5:B7')
 ws1['A5'] = '田中'
+
+# 罫線 #
+s = Side(style='thin')
+b = Borde(lefts, rights, tops, bottoms)
+cell.border = b
+wb.save('output.xlsx')
