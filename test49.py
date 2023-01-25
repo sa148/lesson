ddcd07b50e663eb90b1d89ecdc5a068d8f558828
sa148 <s.kurono@outlook.jp>
2023-01-25 13:54:07 +0900

    addトレースバック

---
+++ test49.py
@@ -0,0 +1,11 @@
+# トレースバック #
+import traceback
+
+try:
+    result = 1 / 0
+    print(result)
+except ZeroDivisionError:
+    print('---トレースバックを出力開始---')
+    t = traceback.format_exc()
+    print(t)
+    print('---トレースバックを出力修了---')
