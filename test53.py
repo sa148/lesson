ba2d4f1f604244c49c9ad226f111bb13700d9b7a
sa148 <s.kurono@outlook.jp>
2023-01-26 03:26:14 +0900

    addワークシートオブジェクト取得

--- test53.py
+++ test53.py
@@ -1,5 +1,9 @@
-# 既存のファイル読み込み #
+# ワークシートオブジェクト取得 #
 from openpyxl import load_workbook
 wb = load_workbook(filename='output.xlsx')
-sheet_names = wb.sheetnames
-print(sheet_names)
+sheet_names = wb.sheetnames[0]
+ws1 = wb[sheet_names]
+x = ws1['A1'].value
+ws2 = wb.create_sheet('new sheet')
+ws2['A2'] = x
+wb.save('output.xlsx')
